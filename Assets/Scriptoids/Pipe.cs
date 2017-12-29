18164daf80c13ccabd7ee4f1f927849d2bc173d8
wbock <wilfried.bock@futurice.com>
2017-12-29 11:37:56 +0200

    pipe down!

---
+++ Assets/Scriptoids/Pipe.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pipe : MonoBehaviour {
+
+	public float curveRadius, pipeRadius;
+	public int curveSegmentCount, pipeSegmentCount;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
