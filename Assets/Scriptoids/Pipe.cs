1ca4f6e1f2d4e6d86b34113efceca52196613d4d
wbock <wilfried.bock@futurice.com>
2017-12-29 13:36:10 +0200

    gizmos

--- Assets/Scriptoids/Pipe.cs
+++ Assets/Scriptoids/Pipe.cs
@@ -2,18 +2,65 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Pipe : MonoBehaviour {
+public class Pipe : MonoBehaviour
+{
 
-	public float curveRadius, pipeRadius;
-	public int curveSegmentCount, pipeSegmentCount;
+    public float curveRadius, pipeRadius;
+    public int curveSegmentCount, pipeSegmentCount;
 
-	// Use this for initialization
-	void Start () {
-		
+    private Mesh mesh;
+    private Vector3[] vertices;
+    private int[] triangles;
+
+    private void Start()
+    {
+        GetComponent<MeshFilter>().mesh = mesh = new Mesh();
+        mesh.name = "Pipe";
+        SetVertices();
+        SetTriangles();
+    }
+
+    private void SetVertices() {
+		vertices = new Vector3[pipeSegmentCount * curveSegmentCount * 4];
+		float uStep = (2f * Mathf.PI) / curveSegmentCount;
+		CreateFirstQuadRing(uStep);
+		mesh.vertices = vertices;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    private void SetTriangles() { }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnDrawGizmos()
+    {
+        float uStep = (2f * Mathf.PI) / curveSegmentCount;
+        float vStep = (2f * Mathf.PI) / pipeSegmentCount;
+
+        for (int u = 0; u < curveSegmentCount; u++)
+        {
+            for (int v = 0; v < pipeSegmentCount; v++)
+            {
+                Vector3 point = GetPointOnTorus(u * uStep, v * vStep);
+                Gizmos.color = new Color(
+                    1f,
+                    (float)v / pipeSegmentCount,
+                    (float)u / curveSegmentCount);
+                Gizmos.DrawSphere(point, 0.1f);
+            }
+        }
+    }
+
+    private Vector3 GetPointOnTorus(float u, float v)
+    {
+        Vector3 p;
+        float r = (curveRadius + pipeRadius * Mathf.Cos(v));
+        p.x = r * Mathf.Sin(u);
+        p.y = r * Mathf.Cos(u);
+        p.z = pipeRadius * Mathf.Sin(v);
+        return p;
+    }
 }
